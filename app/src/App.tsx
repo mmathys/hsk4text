cf1b7469361c92acdef30eaf55c075193ed8433a
Max Mathys <threadox@gmail.com>
2021-08-31 09:34:25 +0200

    clean up a bit

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,114 +1,103 @@
-import React, { useEffect, useRef, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Text } from "./Text";
-import { title } from "process";
-import _ from "lodash";
+import React, { useEffect, useRef, useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { Text } from "./Text"
+import { title } from "process"
+import _ from "lodash"
 
 export type TextConfig = {
-  lesson: number;
-  text: number;
-  title: string | undefined;
-};
+  lesson: number
+  text: number
+  title: string | undefined
+}
 
 type Config = {
-  texts: [TextConfig];
-};
+  texts: [TextConfig]
+}
 
 export function getKey(config: TextConfig | undefined) {
-  if (!config) return;
-  return (config.lesson < 10 ? "0" : "") + config.lesson + "-" + config.text;
+  if (!config) return
+  return (config.lesson < 10 ? "0" : "") + config.lesson + "-" + config.text
 }
 
 function App() {
-  const [config, setConfig] = useState<Config>();
-  const [activeText, setActiveText] = useState<TextConfig>();
-  const [selectedLesson, setSelectedLesson] = useState<number>(-1);
-  const audioRef = useRef<HTMLAudioElement>(null);
+  const [config, setConfig] = useState<Config>()
+  const [activeText, setActiveText] = useState<TextConfig>()
+  const [selectedLesson, setSelectedLesson] = useState<number>(-1)
+  const audioRef = useRef<HTMLAudioElement>(null)
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("config.json");
-      const json: Config = await res.json();
-      setConfig(json);
-    };
+      const res = await fetch("config.json")
+      const json: Config = await res.json()
+      setConfig(json)
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   const setText = (entry: TextConfig) => {
-    console.log(entry);
-    setActiveText(entry);
+    setActiveText(entry)
     if (audioRef.current) {
-      audioRef.current.load();
+      audioRef.current.load()
     }
-  };
+  }
 
   const nav = () => {
-    if (config) {
-      const grouped = _.groupBy(config.texts, (c: TextConfig) => c.lesson);
-      const keys = _.keys(grouped);
-      return keys.map((lesson: string) => {
-        const lesson_num = parseInt(lesson);
-        const entries = grouped[lesson];
-        const subitems = () => {
-          if (lesson_num === selectedLesson) {
-            return entries.map((entry) => {
-              const className =
-                "nav-entry" + (entry === activeText ? " active" : "");
-              return (
-                <div
-                  className={className}
-                  key={getKey(entry)}
-                  onClick={() => setText(entry)}
-                >
-                  Text {entry.text}
-                </div>
-              );
-            });
-          }
-        };
+    if (!config) return
 
-        const className =
-          "nav-lesson" +
-          (lesson_num === activeText?.lesson ? " active" : "");
-        return (
-          <div>
-            <p
-              className={className}
-              onClick={() => {
-                if(lesson_num === selectedLesson) {
-                  setSelectedLesson(-1)
-                } else {
-                  setSelectedLesson(lesson_num)
-                }
-              }}
-            >
-              Lesson {lesson}
-            </p>
-            {subitems()}
-          </div>
-        );
-      });
+    const selectLesson = (lesson_num: number) => () => {
+      if (lesson_num === selectedLesson) {
+        setSelectedLesson(-1)
+      } else {
+        setSelectedLesson(lesson_num)
+      }
     }
-  };
+
+    const subitems = (entries: TextConfig[], lesson_num: number) => {
+      if (lesson_num !== selectedLesson) return
+      const entryClassName = (entry: TextConfig) => "nav-entry" + (entry === activeText ? " active" : "")
+      return entries.map((entry) => (
+        <div className={entryClassName(entry)} key={getKey(entry)} onClick={() => setText(entry)}>
+          Text {entry.text}
+        </div>
+      ))
+    }
+
+    const grouped = _.groupBy(config.texts, (c: TextConfig) => c.lesson)
+    const keys = _.keys(grouped)
+    return keys.map((lesson: string) => {
+      const lesson_num = parseInt(lesson)
+      const entries = grouped[lesson]
+      const className = "nav-lesson" + (lesson_num === activeText?.lesson ? " active" : "")
+      return (
+        <div>
+          <p className={className} onClick={selectLesson(lesson_num)}>
+            Lesson {lesson}
+          </p>
+          {subitems(entries, lesson_num)}
+        </div>
+      )
+    })
+  }
 
   const content = () => {
-    if (activeText) {
-      const title = () =>
-        activeText.title && <p className="title">{activeText.title}</p>;
-      return (
-        <div className="text chinese">
-          {title()}
-          <Text config={activeText}></Text>
-          <audio className="audio" controls ref={audioRef}>
-            <source src={`audio/${getKey(activeText)}.mp3`} type="audio/mpeg" />
-            Your browser does not support the audio element.
-          </audio>
-        </div>
-      );
+    if (!activeText) return
+    const title = () => {
+      if (!activeText.title) return
+      else return <p className="title">{activeText.title}</p>
     }
-  };
+    return (
+      <div className="text chinese">
+        {title()}
+        <Text config={activeText}></Text>
+        <audio className="audio" controls ref={audioRef}>
+          <source src={`audio/${getKey(activeText)}.mp3`} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    )
+  }
 
   return (
     <div className="App">
@@ -117,7 +106,7 @@ function App() {
         {content()}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
